c36ec4377d97a70ff807e35c1d4652a3905a90c7
Daniyar Nasirov <lilbonnie666@gmail.com>
2024-02-10 13:18:58 +0600

    added stylization to quotes category

--- src/Container/Quotes.tsx
+++ src/Container/Quotes.tsx
@@ -1,8 +1,17 @@
 
+
 const Quotes = () => {
     return (
-        <div>
-
+        <div className='flex ml-20 mt-4'>
+            <h3 className="font-bold">Categories:</h3>
+            <ul className="flex ml-20">
+                <li className="ml-10 underline">All</li>
+                <li className="ml-10 underline">Star Wars</li>
+                <li className="ml-10 underline">Famous people</li>
+                <li className="ml-10 underline">Saying</li>
+                <li className="ml-10 underline">Humour</li>
+                <li className="ml-10 underline">Motivational</li>
+            </ul>
         </div>
     );
 };
