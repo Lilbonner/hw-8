b8d475ad731c664c89a93f9bd7cb48500067e6ac
Daniyar Nasirov <lilbonnie666@gmail.com>
2024-02-10 12:57:59 +0600

    added stylization to navbar

--- src/Container/Navbar/Navbar.tsx
+++ src/Container/Navbar/Navbar.tsx
@@ -1,8 +1,13 @@
+import {NavLink} from "react-router-dom";
 
 const Navbar = () => {
     return (
-        <div className="text-2xl text-blue-500">
-Navbar will be here.
+        <div className="flex bg-gray-500 h-16 text-white">
+            <h2 className="text-2xl mt-4 ml-36 font-bold">Quotes Central</h2>
+            <div className="flex text-end ml-64">
+                <NavLink className="text-1xl ml-96 mt-5" to="/" >Quotes </NavLink>
+                <NavLink className="text-1xl ml-16 mt-5" to="/NewQuote" >New quotes</NavLink>
+            </div>
         </div>
     );
 };
