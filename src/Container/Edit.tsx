5531853ffb31d53416089419102224615c5a4edd
Daniyar Nasirov <lilbonnie666@gmail.com>
2024-02-10 14:20:51 +0600

    added stylization to edit form

--- src/Container/Edit.tsx
+++ src/Container/Edit.tsx
@@ -35,27 +35,35 @@ const EditQuote: React.FC<EditQuoteProps> = ({ id, initialCategory = '', initial
     };
 
     return (
-        <div className="quote-card container">
-            <h3>Edit quote</h3>
-            <input
-                type="text"
-                placeholder="Category"
+        <div className="h-96 w-96 border-2 border-black ml-5 rounded-md mt-10 ">
+            <h3 className="font-bold text-2xl ml-2 mt-2">Edit quote:</h3>
+            <select
+                className="ml-2 rounded-md mt-14"
                 value={category}
-                onChange={(e) => setCategory(e.target.value)}
-            />
+                onChange={(e) => setCategory(e.target.value)}>
+                <option value="">Select a category</option>
+                <option value="Star Wars">Star Wars</option>
+                <option value="Famous people">Famous people</option>
+                <option value="Saying">Saying</option>
+                <option value="Humour">Humour</option>
+                <option value="Motivational">Motivational</option>
+            </select>
             <input
-                className="authorInput"
+                className="ml-2 rounded-md mt-6"
                 type="text"
                 placeholder="Author"
                 value={author}
                 onChange={(e) => setAuthor(e.target.value)}
             />
             <textarea
+                className="ml-2 с mt-6 w-80 h-24 rounded-md "
                 placeholder="Quote text"
                 value={text}
                 onChange={(e) => setText(e.target.value)}
             ></textarea>
-            <button onClick={handleSubmit}>Save changes</button>
+            <button
+                className="w-20 mt-6 ml-64 bg-gray-400"
+                onClick={handleSubmit}>Save</button>
         </div>
     );
 };
